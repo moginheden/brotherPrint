5ad1b57747c66b0b51a820998b46586243ba27b3
Brian Heward <brian.heward@creb.ca>
2018-10-24 14:24:18 -0600

    got native reference link working, (by referencing a dll that is ver transient.... not good)
    Now compiles and runs, but doesn't print anything on iOS.

--- PrintTest/PrintTest.iOS/BrotherPrint_iOS.cs
+++ PrintTest/PrintTest.iOS/BrotherPrint_iOS.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 using Foundation;
 using SkiaSharp;
+using SkiaSharp.Views.iOS;
 using BRPtouchPrintKitW;
 
 [assembly: Xamarin.Forms.Dependency(typeof(PrintTest.iOS.BrotherPrint_iOS))]
@@ -15,8 +16,32 @@ namespace PrintTest.iOS
 
         public void print(SKBitmap toPrint)
         {
-            BRPtouchPrinter printer = null;
+            try
+            {
+                BRPtouchPrintInfo printInfo = new BRPtouchPrintInfo();
 
+                printInfo.StrPaperName = @"36mm";
+
+                BRPtouchPrinter printer = new BRPtouchPrinter("Brother PT-P950NW", ConnectionType.Wlan);
+
+                printer.SetIPAddress("192.168.43.126"); //192.168.43.126
+                printer.SetPrintInfo(printInfo);
+
+                bool worked = printer.StartCommunication();
+
+                CoreGraphics.CGImage cgImage2 = new CoreGraphics.CGImage(toPrint.Width, toPrint.Height, 8, toPrint.BytesPerPixel * 8, toPrint.BytesPerPixel * toPrint.Width, CoreGraphics.CGColorSpace.CreateGenericGray(), CoreGraphics.CGBitmapFlags.None, new CoreGraphics.CGDataProvider(toPrint.Bytes), null, false, CoreGraphics.CGColorRenderingIntent.Default);
+
+                using (CoreGraphics.CGImage cgImage = toPrint.ToCGImage())
+                {
+                    printer.PrintImage(cgImage, 1);
+                    printer.PrintImage(cgImage2, 1);
+                }
+                printer.EndCommunication();
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine("ERROR: " + ex.Message + "\r\n" + ex.StackTrace);
+            }
         }
     }
 }
